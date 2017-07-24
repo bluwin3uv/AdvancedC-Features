0c4d7c2d003600b2e65daeef229038d36bcb9ec8
uvblu <uvblu@LAPTOP-KOKCMQF8>
2017-07-24 17:20:51 +1000

    yu

---
+++ Assets/1-Genericcs/Scripts/GenericsTest.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+namespace Generics
+{
+    public class GenericsTest : MonoBehaviour
+    {
+        public float spawnRadius = 50f;
+        public GameObject prefab;
+        public int spawnAmount = 20;
+        public LottyList<GameObject> gameObjects = new LottyList<GameObject>();
+        void Start()
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                GameObject clone = Instantiate(prefab);
+                Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius;
+                clone.transform.position = randomPos;
+                gameObjects.Add(clone);
+            }
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/1-Genericcs/Scripts/LottyList.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Generics
+{
+
+    public class LottyList<T>
+    {
+        public T[] list;
+        public int amount { get; private set; }
+        public LottyList() { amount = 0; }
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+        public void Add(T items)
+        {
+            T[] cache = new T[amount + 1];
+            if(list != null)
+            {
+                for(int i = 0; i < list.Length; i++)
+                {
+                    cache[i] = list[i];
+                }
+                cache[amount] = items;
+                list = cache;
+                amount++;
+            } 
+        }
+    }
+}
